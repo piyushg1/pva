524afe26624ebce2725adcbe7f6e42bfbd9b0046
Piyush <piyushat25@gmail.com>
2023-04-07 12:28:33 +0530

    first

---
+++ src/pages/homepage/home1-dark.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import Navbar from "../../components/Navbar/navbar";
+import IntroWithSlider from "../../components/Intro-with-slider/intro-with-slider";
+import Services from "../../components/Services/services";
+import Works from "../../components/Works/works";
+import Numbers from "../../components/Numbers/numbers";
+import Clients from "../../components/Clients/clients";
+import CallToAction from "../../components/Call-to-action/call-to-action";
+import Footer from "../../components/Footer/footer";
+import DarkTheme from "../../layouts/Dark";
+
+const Homepage1 = () => {
+  const fixedSlider = React.useRef(null);
+  const MainContent = React.useRef(null);
+  const navbarRef = React.useRef(null);
+  const logoRef = React.useRef(null);
+
+  React.useEffect(() => {
+    setInterval(() => {
+      if (fixedSlider.current) {
+        var slidHeight = fixedSlider.current.offsetHeight;
+      }
+      if (MainContent.current) {
+        MainContent.current.style.marginTop = slidHeight + "px";
+      }
+    }, 1000);
+    var navbar = navbarRef.current;
+    if (window.pageYOffset > 300) {
+      navbar.classList.add("nav-scroll");
+    } else {
+      navbar.classList.remove("nav-scroll");
+    }
+    window.addEventListener("scroll", () => {
+      if (window.pageYOffset > 300) {
+        navbar.classList.add("nav-scroll");
+      } else {
+        navbar.classList.remove("nav-scroll");
+      }
+    });
+  }, [fixedSlider, MainContent, navbarRef]);
+
+  return (
+    <DarkTheme>
+      <Navbar nr={navbarRef} lr={logoRef} />
+      <IntroWithSlider sliderRef={fixedSlider} />
+      <div ref={MainContent} className="main-content">
+        <Services />
+        <Works />
+        <Numbers />
+        <Clients theme="dark" />
+        <CallToAction />
+        <Footer />
+      </div>
+    </DarkTheme>
+  );
+};
+
+export default Homepage1;
---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Homepage1 from "./homepage/home1-dark";
+
+const Home = () => {
+  return (
+    <>
+      <Homepage1 />
+    </>
+  );
+};
+
+export default Home;
